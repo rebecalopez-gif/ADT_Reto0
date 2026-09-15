712b2c6410159b1e189fffb5ed8c8ff1700b7e50
ire220507-ctrl <ire220507@gamil.com>
2026-09-15 13:30:06 +0200

    primer commit

---
+++ src/main/java/com/mycompany/adt_reto0/ADT_Reto0.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.adt_reto0;
+
+/**
+ *
+ * @author ire22
+ */
+public class ADT_Reto0 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
