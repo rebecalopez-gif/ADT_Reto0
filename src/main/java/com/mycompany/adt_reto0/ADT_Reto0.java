ca776b791980d69ca418ca902c921bf3d310d401
ProgramaPrincipal.java <galderlindosa@gmail.com>
2026-09-15 14:00:38 +0200

    HOLA MUNDO PRUEBA RAMA

--- src/main/java/com/mycompany/adt_reto0/ADT_Reto0.java
+++ src/main/java/com/mycompany/adt_reto0/ADT_Reto0.java
@@ -11,6 +11,6 @@ package com.mycompany.adt_reto0;
 public class ADT_Reto0 {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("HOLA MUNDO!");
     }
 }
